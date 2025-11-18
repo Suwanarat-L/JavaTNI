6ddad54570dcc6d384852232a5728cc6349ee9bc
Suwanarat-L <suwanarat43938@gmail.com>
2025-11-18 12:41:50 +0700

    commit two java files

---
+++ LabSheet0/src/fistJavaClass.java
@@ -0,0 +1,5 @@
+public class fistJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ LabSheet0/src/secondJavaClass.java
@@ -0,0 +1,6 @@
+public class secondJavaClass {
+    public static void main(String[] args) {
+        System.out.println("INT-1082/DAT-105");
+
+    }
+}

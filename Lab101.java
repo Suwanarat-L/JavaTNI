4c960ca9279ec3c427660af53f2594baaaaa2513
Suwanarat-L <suwanarat43938@gmail.com>
2025-11-18 13:48:06 +0700

    Delete Lab101.java

--- Lab101.java
+++
@@ -1,8 +0,0 @@
-public class Lab101 {
-    public static void main(String[] args) {
-        System.out.println("Hello, I am Suwanarat Luangampornkul");
-        //System.out.println("\"INT-108\" 'Object-Oriented Programming'");
-        System.out.println("\tThis is my fist time learning Java");
-        System.out.println("\tI hope to get grade \"A\"");
-    }
-}
